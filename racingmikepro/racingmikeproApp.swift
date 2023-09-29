b3394fa043c1828d85fe717b200fda1fc761ba67
Michele <micheleberardi@pm.me>
2023-09-29 15:59:16 -0400

    Initial Commit

---
+++ racingmikepro/racingmikeproApp.swift
@@ -0,0 +1,17 @@
+//
+//  racingmikeproApp.swift
+//  racingmikepro
+//
+//  Created by Michele on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct racingmikeproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
